8a709f3c5e2a05edb5bc83c73077f487b28fb690
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-03 10:33:22 +0800

    Build database

---
+++ data/migrations/20190603101008_initial_tables.js
@@ -0,0 +1,25 @@
+
+exports.up = async function(knex) {
+  await knex.schema.createTable('Projects', tbl => {
+    tbl.increments('id')
+    tbl.string('name').unique().notNullable()
+    tbl.string('description', 1000)
+    tbl.boolean('is_complete').notNullable().defaultTo(false)
+  })
+
+  await knex.schema.createTable('Actions', tbl => {
+    tbl.increments('id')
+    tbl
+      .integer('project_id')
+      .references('id')
+      .inTable('Projects')
+    tbl.string('description', 1000).notNullable()
+    tbl.string('notes', 1000)
+    tbl.boolean('is_complete').notNullable().defaultTo(false)
+  })
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('Actions')
+  await knex.schame.dropTableIfExists('Projects')
+};
