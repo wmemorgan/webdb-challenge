f6054ee27633465f84557700f9e48f891aed26cf
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-03 11:30:40 +0800

    Activate project and action resource routes

--- api/server.js
+++ api/server.js
@@ -3,16 +3,21 @@ const helmet = require('helmet')
 const logger = require('morgan')
 
 // Import resource routes
+const projectRoutes = require('../routes/projectRoutes')
+const actionRoutes = require('../routes/actionRoutes')
 
 // Instantiate server
 const server = express()
 
-// Load middleware
+//==== Global middleware ==== //
+const validateProjectId = require('../middleware')
 server.use(helmet())
 server.use(express.json())
 server.use(logger('dev'))
 
 // Activate routes
+server.use(`/api/projects`, projectRoutes)
+server.use(`/api/projects/:id/actions`, validateProjectId, actionRoutes)
 server.use('/', (req, res) => {
   res.send(`<h1>Welcome to the Projects App API server</h1>`)
 })
